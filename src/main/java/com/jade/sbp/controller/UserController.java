abf8bb86f2ac2219942d505fffa9b7a95ca8b65f
indiflex <indiflex1@gmail.com>
2018-10-18 15:32:57 +0900

    aaa

--- src/main/java/com/jade/sbp/controller/UserController.java
+++ src/main/java/com/jade/sbp/controller/UserController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.jade.sbp.domain.User;
 import com.jade.sbp.mapper.UserMapper;
@@ -15,6 +17,13 @@ public class UserController {
 	
 	@Autowired
 	private UserMapper mapper;
+	
+	@RequestMapping(value="/korean", method=RequestMethod.POST)
+	public String korean(@RequestParam String name, Model model) throws Exception {
+		System.out.println("name=" + name);
+		model.addAttribute("name", name);
+		return "hello2";
+	}
 	
 	@RequestMapping("/hello2")
 	public void hello2(Model model) throws Exception {
